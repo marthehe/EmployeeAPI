c413334a76e3f1d384c7e80571234a4fcc76ad18
Marta <marta.hendel@gmail.com>
2022-03-23 20:19:27 +0000

    Added Create token functionality

--- EmployeeApp/serializers.py
+++ EmployeeApp/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from EmployeeApp.models import Departments, Employees
 from django.contrib.auth.models import User
+from rest_framework.authtoken.views import Token
 
 
 class DepartmentSerializer(serializers.ModelSerializer):
@@ -19,3 +20,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'password']
+
+        extra_kwargs = {'password': {
+            'write_only': True,
+            'required': True
+        }}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
