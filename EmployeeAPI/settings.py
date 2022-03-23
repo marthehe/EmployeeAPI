ec8dfbb32462f521695bb61cc24091644cfd69f7
Marta <marta.hendel@gmail.com>
2022-03-23 16:59:04 +0000

    Added authentication settings

--- EmployeeAPI/settings.py
+++ EmployeeAPI/settings.py
@@ -38,9 +38,21 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
-    'EmployeeApp.apps.EmployeeappConfig'
+    'EmployeeApp.apps.EmployeeappConfig',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+
+    ],
+
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
 
 CORS_ORIGIN_ALLOW_ALL = True
 
