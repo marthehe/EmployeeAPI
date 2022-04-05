86f828cb4d765cf4e3f772d7f3e51882006255ac
Marta <marta.hendel@gmail.com>
2022-04-05 14:43:59 +0100

    Refactor Department component

--- ui/employee-app/src/Department.js
+++ ui/employee-app/src/Department.js
@@ -1,386 +1,149 @@
-import React, { Component } from "react";
-import { variables } from "./Variables.js";
+import { useState, useEffect } from "react";
+import DepartmentForm from "./DepartmentForm";
+import DepartmentAPIService from "./DepartmentAPIService";
+import { useCookies } from "react-cookie";
 
-const headers = new Headers();
-headers.append(
-  "Authorization",
-  "Token f6ef24924526ee0c17a6f5ba0e87c108afebd6cd"
-);
+function Department() {
+  const [departments, setDepartments] = useState([]);
+  const [editDepartment, setEditDepartment] = useState(null);
+  const [token] = useCookies(["mytoken"]);
 
-export class Department extends Component {
-  constructor(props) {
-    super(props);
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/department/", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${token["mytoken"]}`,
+      },
+    })
+      .then((resp) => resp.json())
+      .then((resp) => setDepartments(resp))
+      .catch((error) => console.log(error));
+  }, []);
 
-    this.state = {
-      departments: [],
-      modalTitle: "",
-      DepartmentName: "",
-      DepartmentId: 0,
+  const editClick = (department) => {
+    setEditDepartment(department);
+  };
 
-      DepartmentIdFilter: "",
-      DepartmentNameFilter: "",
-      departmentsWithoutFilter: [],
-    };
-  }
-
-  FilterFn() {
-    var DepartmentIdFilter = this.state.DepartmentIdFilter;
-    var DepartmentNameFilter = this.state.DepartmentNameFilter;
-
-    var filteredData = this.state.departmentsWithoutFilter.filter(function (
-      el
-    ) {
-      return (
-        el.DepartmentId.toString()
-          .toLowerCase()
-          .includes(DepartmentIdFilter.toString().trim().toLowerCase()) &&
-        el.DepartmentName.toString()
-          .toLowerCase()
-          .includes(DepartmentNameFilter.toString().trim().toLowerCase())
-      );
-    });
-
-    this.setState({ departments: filteredData });
-  }
-
-  sortResult(prop, asc) {
-    var sortedData = this.state.departmentsWithoutFilter.sort(function (a, b) {
-      if (asc) {
-        return a[prop] > b[prop] ? 1 : a[prop] < b[prop] ? -1 : 0;
+  const updatedDepartmentInfo = (department) => {
+    const new_depinfo = departments.map((mydepinfo) => {
+      if (mydepinfo.DepartmentId === department.DepartmentId) {
+        return department;
       } else {
-        return b[prop] > a[prop] ? 1 : b[prop] < a[prop] ? -1 : 0;
+        return mydepinfo;
       }
     });
 
-    this.setState({ departments: sortedData });
-  }
-
-  changeDepartmentIdFilter = (e) => {
-    this.state.DepartmentIdFilter = e.target.value;
-    this.FilterFn();
-  };
-  changeDepartmentNameFilter = (e) => {
-    this.state.DepartmentNameFilter = e.target.value;
-    this.FilterFn();
+    setDepartments(new_depinfo);
   };
 
-  refreshList() {
-    var requestOptions = {
-      method: "GET",
-      headers: headers,
-    };
-    fetch(variables.API_URL + "department", requestOptions)
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({ departments: data, departmentsWithoutFilter: data });
+  const departmentForm = () => {
+    setEditDepartment({
+      Department: "",
+    });
+  };
+
+  const insertedDepartmentInfo = (department) => {
+    const new_department = [...departments, department];
+    setDepartments(new_department);
+  };
+
+  const deleteDepartment = (department) => {
+    DepartmentAPIService.DeleteDepartment(
+      department.DepartmentId,
+      token["mytoken"]
+    ).then(() => {
+      const new_department = departments.filter((mydepartment) => {
+        if (mydepartment.DepartmentId === department.DepartmentId) {
+          return false;
+        }
+
+        return true;
       });
-  }
 
-  componentDidMount() {
-    this.refreshList();
-  }
-
-  changeDepartmentName = (e) => {
-    this.setState({ DepartmentName: e.target.value });
+      setDepartments(new_department);
+    });
   };
 
-  addClick() {
-    this.setState({
-      modalTitle: "Add Department",
-      DepartmentId: 0,
-      DepartmentName: "",
-    });
-  }
-  editClick(dep) {
-    this.setState({
-      modalTitle: "Edit Department",
-      DepartmentId: dep.DepartmentId,
-      DepartmentName: dep.DepartmentName,
-    });
-  }
-
-  createClick() {
-    fetch(variables.API_URL + "department", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        DepartmentName: this.state.DepartmentName,
-      }),
-    })
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          alert(result);
-          this.refreshList();
-        },
-        (error) => {
-          alert("Failed");
-        }
-      );
-  }
-
-  updateClick() {
-    fetch(variables.API_URL + "department", {
-      method: "PUT",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        DepartmentId: this.state.DepartmentId,
-        DepartmentName: this.state.DepartmentName,
-      }),
-    })
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          alert(result);
-          this.refreshList();
-        },
-        (error) => {
-          alert("Failed");
-        }
-      );
-  }
-
-  deleteClick(id) {
-    if (window.confirm("Are you sure?")) {
-      fetch(variables.API_URL + "department/" + id, {
-        method: "DELETE",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      })
-        .then((res) => res.json())
-        .then(
-          (result) => {
-            alert(result);
-            this.refreshList();
-          },
-          (error) => {
-            alert("Failed");
-          }
-        );
-    }
-  }
-
-  render() {
-    const { departments, modalTitle, DepartmentId, DepartmentName } =
-      this.state;
-
-    return (
-      <div>
-        <button
-          type="button"
-          className="btn btn-primary m-2 float-end"
-          data-bs-toggle="modal"
-          data-bs-target="#exampleModal"
-          onClick={() => this.addClick()}
-        >
-          Add Department
-        </button>
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th>
-                <div className="d-flex flex-row">
-                  <input
-                    className="form-control m-2"
-                    onChange={this.changeDepartmentIdFilter}
-                    placeholder="Filter"
-                  />
-
-                  <button
-                    type="button"
-                    className="btn btn-light"
-                    onClick={() => this.sortResult("DepartmentId", true)}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-arrow-down-square-fill"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0z" />
-                    </svg>
-                  </button>
-
-                  <button
-                    type="button"
-                    className="btn btn-light"
-                    onClick={() => this.sortResult("DepartmentId", false)}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-arrow-up-square-fill"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z" />
-                    </svg>
-                  </button>
-                </div>
-                DepartmentId
-              </th>
-              <th>
-                <div className="d-flex flex-row">
-                  <input
-                    className="form-control m-2"
-                    onChange={this.changeDepartmentNameFilter}
-                    placeholder="Filter"
-                  />
-
-                  <button
-                    type="button"
-                    className="btn btn-light"
-                    onClick={() => this.sortResult("DepartmentName", true)}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-arrow-down-square-fill"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0z" />
-                    </svg>
-                  </button>
-
-                  <button
-                    type="button"
-                    className="btn btn-light"
-                    onClick={() => this.sortResult("DepartmentName", false)}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-arrow-up-square-fill"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z" />
-                    </svg>
-                  </button>
-                </div>
-                DepartmentName
-              </th>
-              <th>Options</th>
-            </tr>
-          </thead>
-          <tbody>
-            {departments.map((dep) => (
-              <tr key={dep.DepartmentId}>
-                <td>{dep.DepartmentId}</td>
-                <td>{dep.DepartmentName}</td>
-                <td>
-                  <button
-                    type="button"
-                    className="btn btn-light mr-1"
-                    data-bs-toggle="modal"
-                    data-bs-target="#exampleModal"
-                    onClick={() => this.editClick(dep)}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-pencil-square"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                      <path
-                        fillRule="evenodd"
-                        d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
-                      />
-                    </svg>
-                  </button>
-
-                  <button
-                    type="button"
-                    className="btn btn-light mr-1"
-                    onClick={() => this.deleteClick(dep.DepartmentId)}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-trash-fill"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
-                    </svg>
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-
-        <div
-          className="modal fade"
-          id="exampleModal"
-          tabIndex="-1"
-          aria-hidden="true"
-        >
-          <div className="modal-dialog modal-lg modal-dialog-centered">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">{modalTitle}</h5>
+  return (
+    <div>
+      <button
+        type="button"
+        className="btn btn-primary m-2 float-end"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+        onClick={departmentForm}
+      >
+        Add Department
+      </button>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>DepartmentId</th>
+            <th>DepartmentName</th>
+            <th>Options</th>
+          </tr>
+        </thead>
+        <tbody>
+          {departments.map((department) => (
+            <tr key={department.DepartmentId}>
+              <td>{department.DepartmentId}</td>
+              <td>{department.DepartmentName}</td>
+              <td>
                 <button
                   type="button"
-                  className="btn-close"
-                  data-bs-dismiss="modal"
-                  aria-label="Close"
-                ></button>
-              </div>
-
-              <div className="modal-body">
-                <div className="input-group mb-3">
-                  <span className="input-group-text">DepartmentName</span>
-                  <input
-                    type="text"
-                    className="form-control"
-                    value={DepartmentName}
-                    onChange={this.changeDepartmentName}
-                  />
-                </div>
-
-                {DepartmentId === 0 ? (
-                  <button
-                    type="button"
-                    className="btn btn-primary float-start"
-                    onClick={() => this.createClick()}
+                  className="btn btn-light mr-1"
+                  data-bs-toggle="modal"
+                  data-bs-target="#exampleModal"
+                  onClick={() => editClick(department)}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-pencil-square"
+                    viewBox="0 0 16 16"
                   >
-                    Create
-                  </button>
-                ) : null}
+                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                    <path
+                      fillRule="evenodd"
+                      d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
+                    />
+                  </svg>
+                </button>
 
-                {DepartmentId !== 0 ? (
-                  <button
-                    type="button"
-                    className="btn btn-primary float-start"
-                    onClick={() => this.updateClick()}
+                <button
+                  type="button"
+                  className="btn btn-light mr-1"
+                  onClick={() => deleteDepartment(department)}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-trash-fill"
+                    viewBox="0 0 16 16"
                   >
-                    Update
-                  </button>
-                ) : null}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+                  </svg>
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <br />
+      {editDepartment ? (
+        <DepartmentForm
+          department={editDepartment}
+          updatedDepartmentInfo={updatedDepartmentInfo}
+          insertedDepartmentInfo={insertedDepartmentInfo}
+        />
+      ) : null}
+    </div>
+  );
 }
+
+export default Department;
